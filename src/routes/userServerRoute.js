ddf43c883d909c47b8b3a51c73a3cbf50e5fe342
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2024-07-04 16:01:33 +0530

    changed package.json start script

--- src/routes/userServerRoute.js
+++ src/routes/userServerRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const userServerRoute = express.Router();
 
+userServerRoute.use("/hello1", require("./../subServers/hello1/index"));
 module.exports = userServerRoute;
