2deb8e1e2071577f9b5b50005275800ca99e83de
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2024-07-04 17:28:39 +0530

    test

--- src/controller/userControler.js
+++ src/controller/userControler.js
@@ -4,30 +4,30 @@ const user = require("../models/userModel");
 const registerUser = async (req, res) => {
   // console.log("controller");
   try {
-      const { username, name, email, m_number, password } = req.body;
-    //   let c = await user.find({ email: email });
-    //   if (c[0] === undefined) {
-    //     const a = await user.create({
-    //       username,
-    //       name,
-    //       email,
-    //       m_number,
-    //       password,
-    //       created_at: new Date().toLocaleString(),
-    //       updated_at: new Date().toLocaleString(),
-    //     });
-    //     createUserServer(a)
-    //     res.status(201).send(a);
-    //   } else {
-    //     res.send("User Already Exit");
-    //   }
-    let a = { username, name, email, m_number, password };
-    await createUserServer(a);
-    res.send("server Created");
+    const { username, name, email, m_number, password } = req.body;
+    let c = await user.find({ email: email });
+    if (c[0] === undefined) {
+      const a = await user.create({
+        username,
+        name,
+        email,
+        m_number,
+        password,
+        created_at: new Date().toLocaleString(),
+        updated_at: new Date().toLocaleString(),
+      });
+
+      res.status(201).send(a);
+      createUserServer(a)
+    } else {
+      res.send("User Already Exit");
+    }
   } catch (e) {
-    // console.log(e);
+    console.log(e);
     res.status(500).send(e);
   }
 };
+
+
 
 module.exports = { registerUser };
