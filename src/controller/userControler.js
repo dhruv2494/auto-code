67b262d10ca390e209130e97e8e357ec5d546ab6
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2024-07-04 16:33:12 +0530

    deploy test

--- src/controller/userControler.js
+++ src/controller/userControler.js
@@ -4,30 +4,28 @@ const user = require("../models/userModel");
 const registerUser = async (req, res) => {
   // console.log("controller");
   try {
-    const { username, name, email, m_number, password } = req.body;
-    let c = await user.find({ email: email });
-    if (c[0] === undefined) {
-      const a = await user.create({
-        username,
-        name,
-        email,
-        m_number,
-        password,
-        created_at: new Date().toLocaleString(),
-        updated_at: new Date().toLocaleString(),
-      });
-
-      res.status(201).send(a);
-      createUserServer(a)
-    } else {
-      res.send("User Already Exit");
-    }
+    //   const { username, name, email, m_number, password } = req.body;
+    //   let c = await user.find({ email: email });
+    //   if (c[0] === undefined) {
+    //     const a = await user.create({
+    //       username,
+    //       name,
+    //       email,
+    //       m_number,
+    //       password,
+    //       created_at: new Date().toLocaleString(),
+    //       updated_at: new Date().toLocaleString(),
+    //     });
+    //     createUserServer(a)
+    //     res.status(201).send(a);
+    //   } else {
+    //     res.send("User Already Exit");
+    //   }
+    createUserServer(a);
   } catch (e) {
     // console.log(e);
     res.status(500).send(e);
   }
 };
-
-
 
 module.exports = { registerUser };
