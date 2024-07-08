a52ce2ee56a748a92c273c5b2bad6bc5563165dd
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2024-07-08 09:51:52 +0530

    test1 changes

--- src/routes/userServerRoute.js
+++ src/routes/userServerRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
 const userServerRoute = express.Router();
 
-userServerRoute.use("/hello", require("./../subServers/hello/index"));
 module.exports = userServerRoute;
